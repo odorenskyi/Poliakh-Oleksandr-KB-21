af74d7663ad2925fdb857e37c18669b429f090ee
Oleksandr Polyah <93218224+AleksandrPolyah@users.noreply.github.com>
2022-03-22 15:31:16 +0200

    Delete ModulesPoliakh

--- lab08/ModulesPoliakh/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <cmath>
-#include "ModulesPoliakh.h"
-
-
-double s_calculation(double x,double y,double z){
-    return = 2.72 * abs(sin(z)) + pow(0.5,pow(x,2)) - sqrt(abs(pow(y + z,2) - pow(x,5))) / 10 * 3.14 * pow(z,4);
-
-}
